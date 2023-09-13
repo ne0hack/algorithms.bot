ef5aed4581cf67889f99f6bd770f84bb4fc63a31
is it matter? <dev.veeskela@gmail.com>
2023-09-13 18:16:51 +0300

    removed extra spaces

--- src/data.py
+++ src/data.py
@@ -31,9 +31,9 @@ def get_unsolved_algorithms(solved_algorithms: List[str]) -> Dict[str, list]:
         data = response.json()
         for algorithm in data["stat_status_pairs"]:
             if not algorithm["paid_only"]:
-                title = str(algorithm["stat"]["frontend_question_id"]) + ". " + algorithm["stat"]["question__title"]
+                title = str(algorithm["stat"]["frontend_question_id"]) + ". " + algorithm["stat"]["question__title"].strip()
                 status = status_codes[int(algorithm["difficulty"]["level"])]
-                link = "https://leetcode.com/problems/" + algorithm["stat"]["question__title_slug"]
+                link = "https://leetcode.com/problems/" + algorithm["stat"]["question__title_slug"].strip()
 
                 if title not in solved_algorithms:
                     unsolved[status].append({"title": title, "link": link})
